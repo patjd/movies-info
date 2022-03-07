2054e8540629ce3f45aed49d7e49d8225225e0bc
Jaydeep Patel <jaydeep7894@gmail.com>
2022-03-07 16:13:25 -0500

    added movies and genre model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+STATUS_CHOICES = (
+    ('RA', 'RECENTLY ADDED'),
+    ('MW', 'MOST WATCHED'),
+    ('TR', 'TOP RATED'),
+)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    image = models.ImageField(upload_to='movies')
+    banner = models.ImageField(upload_to='movies_banner')
+    genres = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    language = models.CharField(max_length=10)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=2)
+    cast = models.CharField(max_length=100)
+    released_date = models.DateField()
+    runtime = models.IntegerField()
+    movie_trailer = models.URLField()
+
+
+
